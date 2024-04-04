0acd16784f66220372a73b1bf61eec63a92b912c
Alessandro Volpe <37452470+Syro98@users.noreply.github.com>
2024-04-04 23:13:43 +0300

    rand

--- PasswordMaskAnalyzer/Main.py
+++ PasswordMaskAnalyzer/Main.py
@@ -4,6 +4,7 @@ from AnalyzerFunctions import AnalizerFunctions
 @click.command()
 @click.argument('file_name', type=click.Path(exists=True, readable=True), nargs=1)
 @click.option('--top', required=True, type=int)
+
 def main(top, file_name):
     af = AnalizerFunctions()
     with open(file_name, 'r') as f:
